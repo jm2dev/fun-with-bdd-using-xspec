2cd633a60685b4aa2dfbffee3f0a12724e0528c0
jm2dev <jomarcar@gmail.com>
2013-12-18 23:43:27 +0000

    Counting words.

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-ant -Dxspec.xml=tutorial/escape-for-regex.xspec
-ant -Dxspec.xml=hello/hello-world.xspec
+#ant -Dxspec.xml=tutorial/escape-for-regex.xspec
+#ant -Dxspec.xml=hello/hello-world.xspec
+ant -Dxspec.xml=word-frequencies/word-frequencies.xspec
