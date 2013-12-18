7ec94781dcb8313e10822135f9b6957cd8e40d63
jm2dev <jomarcar@gmail.com>
2013-12-18 23:52:50 +0000

    Average book price.

--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@
 
 #ant -Dxspec.xml=tutorial/escape-for-regex.xspec
 #ant -Dxspec.xml=hello/hello-world.xspec
-ant -Dxspec.xml=word-frequencies/word-frequencies.xspec
+#ant -Dxspec.xml=word-frequencies/word-frequencies.xspec
+ant -Dxspec.xml=misc/average-book-price.xspec
